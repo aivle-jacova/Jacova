a9cf01953c419bb3e7e5f05aa3dd7b0d5fbc111d
inseo <in208@naver.com>
2025-05-05 20:22:06 +0900

    모의고사사

---
+++ inseo/Lv_1/모의고사.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] p1 = {1, 2, 3, 4, 5};
+        int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        int count1 = 0, count2 = 0, count3 = 0;
+        
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == p1[i % p1.length]) count1++;
+            if (answers[i] == p2[i % p2.length]) count2++;
+            if (answers[i] == p3[i % p3.length]) count3++;
+        }
+        
+        int maxScore = Math.max(count1, Math.max(count2, count3));
+        List<Integer> result = new ArrayList<>();
+        
+        if (count1 == maxScore) result.add(1);
+        if (count2 == maxScore) result.add(2);
+        if (count3 == maxScore) result.add(3);
+        
+        return result.stream().mapToInt(i->i).toArray();
+    }
+}
