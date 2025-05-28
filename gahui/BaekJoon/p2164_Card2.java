128bf5034e15fe099ca1434620244016cd43d326
gahui <gahui22@naver.com>
2025-05-28 10:28:33 +0900

    큐_250527

--- gahui/BaekJoon/p2164_Card2.java
+++ gahui/BaekJoon/p2164_Card2.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class p2164_카드2 {
+public class p2164_Card2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -10,7 +10,7 @@ public class p2164_카드2 {
 
         int N = sc.nextInt();
 
-        for (int i=1; i<=N; i++){ 
+        for (int i=1; i<=N; i++){
             myQueue.add(i);
         }
         while (myQueue.size()>1) {
