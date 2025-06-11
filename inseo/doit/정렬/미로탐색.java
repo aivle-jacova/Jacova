55381eb9cf93eeae85938605484c032c9de96ef7
inseo <in208@naver.com>
2025-06-11 21:36:51 +0900

    0611

---
+++ inseo/doit/정렬/미로탐색.java
@@ -0,0 +1,18 @@
+import java.awt.Point;
+import java.util.*;
+
+public class 미로탐색 {
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    int [] A=new int[N];
+    for(int i=0;i<N;i++){
+        A[i]=sc.nextInt();
+    }
+
+    Arrays.sort(A);
+    int M=sc.nextInt();
+    for(int i=0;i<M;i++){
+        boolean find=false;
+        int target=sc.nextInt();
+    }
+}
